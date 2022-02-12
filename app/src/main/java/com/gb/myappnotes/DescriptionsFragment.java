07231e1b9fb1383c5655a2c45ab8b129e6db1db5
Калпакбаев <kozha_1992@mail.ru>
2022-02-13 03:39:20 +0500

    Нерозбириха со цветами

---
+++ app/src/main/java/com/gb/myappnotes/DescriptionsFragment.java
@@ -0,0 +1,33 @@
+package com.gb.myappnotes;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class DescriptionsFragment extends Fragment {
+
+
+    public static DescriptionsFragment newInstance(String param1, String param2) {
+        DescriptionsFragment fragment = new DescriptionsFragment();
+        Bundle args = new Bundle();
+        return fragment;
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_descriptions, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        
+    }
+}
--- app/src/main/java/com/gb/myappnotes/MainActivity.java
+++ app/src/main/java/com/gb/myappnotes/MainActivity.java
@@ -2,6 +2,7 @@ package com.gb.myappnotes;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.res.Configuration;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,5 +11,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        if (savedInstanceState==null){
+            NotesFragment notesFragment = new NotesFragment();
+            getSupportFragmentManager().beginTransaction().replace(R.id.notes,notesFragment).commit();
+            if (getResources().getConfiguration().orientation== Configuration.ORIENTATION_LANDSCAPE){
+                DescriptionsFragment descriptionsFragment = new DescriptionsFragment();
+                getSupportFragmentManager().beginTransaction().replace(R.id.descriptions,descriptionsFragment).commit();
+            }
+        }
     }
 }
